26e240f76624383558e96f8cb688e49532f5be14
jeevitha344 <jeevithasagana3131@gmail.com>
2025-12-13 15:01:45 +0530

    fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
               console.log(request)
 setMovie(
   request.data.results[
-    Math.floor(Math.random() * request.data.results.length-1)
+    Math.floor(Math.random() * request.data.results.length)
   ]
 )
     return request;
