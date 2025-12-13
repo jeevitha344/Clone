030912eef68b93d6aa3b601683e0e1b201539379
jeevitha344 <jeevithasagana3131@gmail.com>
2025-12-13 15:36:09 +0530

    fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,6 +8,8 @@ const Banner = () => {
               const fetchData= async() => {
               const request = await axios.get(requests.fetchNetflixOriginals)
               console.log(request)
+               const results = res.data?.results;
+        if (!results || results.length === 0) return;
 setMovie(
   request.data.results[
     Math.floor(Math.random() * request.data.results.length)
